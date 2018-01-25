04269814e864c566ca9b5bdc0e004695de937833
efreneau <33821679+efreneau@users.noreply.github.com>
2018-01-25 13:41:08 -0800

    Add files via upload

--- Matlab/Calculating_RMS_and_SEL2.m
+++ Matlab/Calculating_RMS_and_SEL2.m
@@ -13,7 +13,7 @@ load('MGL1212_Line_AT.mat','-mat')
 f1 = flipud(Data1')*1e6;
 sos=[1,-2,1,1,-1.82570619168342,0.881881926844246;1,-2,1,1,-1.65627993129105,0.707242535896459;1,-2,1,1,-1.57205200320457,0.620422971870477];
 
-fData = sosfilt(sos,f1,2);%2
+fData = sosfilt(sos,f1,2);
 fData = amp(6)*fData;
 
 winData = [];
@@ -22,36 +22,29 @@ T90 = [];
 RMS = [];
 SEL = [];
 
+
 %Find peaks, window around peaks and calculate T90.
 for r=1:size(fData,1)
     row = fData(r,:);
     [val,peak1] = max(row);
-
     if peak1 <= 2*fs%Region 1: Peak is too close to the first index
-        DATA = row(1:peak1+2*fs+1);
-        DATA = [zeros(1,4*fs + 1 - length(DATA)),DATA]
-        disp(length(DATA))
-        T90 = [T90,t90(DATA)];%r
-
+        %DATA = row(1:peak1+2*fs);
+        %T90 = [T90,t90r(DATA)];
+        %DATA = [zeros(1,4*fs + 1 - length(DATA)),DATA];
     elseif peak1 > 2*fs && length(row) - peak1>=1000%Region 2: Peak has space on either side
         DATA = row(peak1-2*fs:peak1+2*fs);
-        T90 = [T90,t90(row)];
+        T90 = [T90,t90(DATA)];
     else %Region 3: Peak is too close to the end
-        %DATA = row(peak1:end);
-        DATA = row(peak-2*fs-1:end)
-        DATA = [DATA, zeros(1,4*fs + 1 - length(DATA))];
+        DATA = row(peak1:end);
         T90 = [T90,t90l(DATA)];
-
+        DATA = [DATA, zeros(1,4*fs + 1 - length(DATA))];
     end
     winData = [winData;DATA];
     peak = [peak,peak1];
-
-
-    
 end
 
 %plot(peak)
-%plot(T90)
+plot(T90)
 
 
 squaredPressure = winData.*winData;
@@ -68,17 +61,22 @@ for r=1:size(RMS,2)%SEL
 end 
 
 
-%plot(T90)
+plot(RMS)
+
 %Write to CSV (https://www.mathworks.com/help/matlab/ref/csvread.html)
 %https://www.mathworks.com/help/matlab/ref/csvwrite.html
+
+%dlmwrite('Data.csv',[depth,date,time,x_gun,y_gun,z_gun,x_r,y_r,z_r,sel,rms],'delimiter',',');
+
+
 
 function tnin=t90(x);%t90 calculation for normal window
     fs = 500;
     tnin = -9999;
     total = sum(x);
-    peak = ceil(length(x)/2);
+    peak2 = ceil(length(x)/2);
     for i=(1:100000)
-        if sum(x(peak-i:peak+i))>=0.9*total
+        if sum(x(peak2-i:peak2+i))>=0.9*total
             tnin = 2*i/fs;
             return;
         end
