8f2e40530e14ae953355f284ed7013d8f543859a
efreneau <33821679+efreneau@users.noreply.github.com>
2018-02-01 00:19:50 -0800

    Add files via upload

--- Matlab/main.m
+++ Matlab/main.m
@@ -1,1 +1,5 @@
+clear all; close all; clc;
+%dataFile = 'R000179_1342879566.RAW';
+%P190 = 'MGL1212NTMCS01.mat';
+%readMCS(strcat('Data/Line AT/',dataFile),strcat('P190/',P190),'Results.mat');
 createCSV('R000179_1342879566.RAW','MGL1212NTMCS01.mat')
