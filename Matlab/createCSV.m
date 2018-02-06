77587607a33dcec594ce0f6b4353044091b710e2
efreneau <33821679+efreneau@users.noreply.github.com>
2018-02-06 11:18:01 -0800

    Option for different deliminators in file location
    Added support for other operating systems.

--- Matlab/createCSV.m
+++ Matlab/createCSV.m
@@ -1,10 +1,13 @@
-function c = createCSV(dataFile,P190)
-%createCSV(dataFile,P190)
+function c = createCSV(dataFile,P190,delim)
+%createCSV(dataFile,P190,delim)
 %
 %createCSV computes the RMS and SEL power for the siesmic streamer data and
 %outputs those and the navigation data found in the P190 to a CSV file. 
 %createCSV depends on readMCS, readP190 and readP190 being in the same
 %directory as this function to work.
+%
+%delim is the delimiator for the operating system you are running this on.
+%Unix based systems use '/' while windows uses '/'.
 %
 %The csv file is created in a directory on the same level as the Data
 %directory. The name of the file will be 'Line Name_Folder Name_file name.csv'
@@ -12,8 +15,8 @@ function c = createCSV(dataFile,P190)
 %example usage: createCSV('Matlab\Data\Line AT\R000179_1342879566.RAW','P190\MGL1212NTMCS01.mat')
 %For more information see github.com/efreneau/machinelearninguw
     fs = 500;
-    dataFileloc = strsplit(dataFile,'\');
-    P190loc = strsplit(P190,'\');
+    dataFileloc = strsplit(dataFile,delim);
+    P190loc = strsplit(P190,delim);
     readMCS(dataFile,P190,'Results.mat');
     load('Results.mat');
     f1 = Data1'*1e6;%unflipped
@@ -61,7 +64,7 @@ function c = createCSV(dataFile,P190)
         SEL = [SEL,sel];
     end 
 
-    csv_dir = strcat(strjoin(P190loc(1:end-3),'\'),'\CSV\');
+    csv_dir = strcat(strjoin(P190loc(1:end-3),delim),strcat(delim,'CSV',delim));
     csv_file = strcat(csv_dir,strjoin(dataFileloc(end-2:end),'_'));
     csv_file = strcat(csv_file(1:end-3),'csv');
     
