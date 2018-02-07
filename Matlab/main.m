01ac45a20cbc4306b40597a2ce2ca097e80f33a7
efreneau <33821679+efreneau@users.noreply.github.com>
2018-02-07 00:00:21 -0800

    Add files via upload

--- Matlab/main.m
+++ Matlab/main.m
@@ -1,8 +1,9 @@
-clear all; close all; clc;
-
 path1 ='C:\Users\zomege\Documents\Machine Learning\Matlab\Data\Line 06\Tape 73\';
 P190 = 'C:\Users\zomege\Documents\Machine Learning\Matlab\P190\MGL1212MCS06.mat';
 csv_location = 'C:\Users\zomege\Documents\Machine Learning\Matlab';
+
+createCSV_reel(path1,P190,csv_location)
+
 %{
 for i=(1:length(reels))
     name = reels(i).name;
@@ -11,11 +12,3 @@ for i=(1:length(reels))
     if(startsWith(name,'tape','IgnoreCase',true))
        cd path
 %}
-files = dir(path1);
-for i=(1:length(files))
-    file = files(i).name;
-    if(endsWith(file,'raw','IgnoreCase',true))
-        location = strcat(path1,file);
-        createCSV(location,P190,csv_location)
-    end
-end
