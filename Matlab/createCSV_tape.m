84e37e7d8d313a4c442fe14ab971be5ba2c837ab
efreneau <33821679+efreneau@users.noreply.github.com>
2018-02-07 17:25:28 -0800

    Add files via upload

--- Matlab/createCSV_tape.m
+++ Matlab/createCSV_tape.m
@@ -5,7 +5,7 @@ maxNumCompThreads(512);
 files = dir(path1);
 for i=(1:length(files))
     file = files(i).name;
-    if(endsWith(file,'raw','IgnoreCase',true))
+    if(endsWith(file,'raw','IgnoreCase',true) && strcmp(file,'EOT')==0)
         location = strcat(path1,file);
         createCSV(location,P190,csv_location)
     end
