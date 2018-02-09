bfb095fceb692a872b91f8fe993bdda9bdb9bc30
efreneau <33821679+efreneau@users.noreply.github.com>
2018-02-08 20:33:46 -0800

    Add files via upload
    main is able to write CSVs for all data in a line. Ignores files that aren't the raw format and are not EOT.

--- Matlab/createCSV_tape.m
+++ Matlab/createCSV_tape.m
@@ -5,8 +5,8 @@ maxNumCompThreads(512);
 files = dir(path1);
 for i=(1:length(files))
     file = files(i).name;
-    if(endsWith(file,'raw','IgnoreCase',true) && strcmp(file,'EOT')==0)
-        location = strcat(path1);
-        createCSV(location,P190,csv_location)
+    if(endsWith(file,'raw','IgnoreCase',true) && strcmp(file,'EOT.RAW')==0)
+        location = strcat(path1,file);
+        createCSV(location,P190,csv_location);
     end
 end
