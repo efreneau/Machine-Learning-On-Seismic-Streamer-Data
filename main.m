45e120f236ebdfbb784ee514761d7da0aa0c7d63
efreneau <33821679+efreneau@users.noreply.github.com>
2018-02-09 20:22:27 -0800

    Add files via upload

---
+++ main.m
@@ -0,0 +1,32 @@
+maxNumCompThreads(512);
+
+%path1 ='C:\Users\zomege\Documents\Machine Learning\Matlab\Data\Line 06\Tape 76\';
+P190 = 'C:\Users\zomege\Documents\Machine Learning\Matlab\P190\MGL1212MCS06.mat';
+csv_location = 'C:\Users\zomege\Documents\Machine Learning\Matlab';
+path1 = 'C:\Users\zomege\Documents\Machine Learning\Matlab\Data\Line 06\TAPE0073.REEL\';
+if ispc %Choose path deliminator
+    delim = '\';
+else
+    delim = '/';
+end
+
+tic
+createCSV_tape(path1,P190,csv_location)
+toc
+
+%Tape 73 is 4.5 Gb and takes 25 minutes to complete and writes 10 mb of output CSVs,
+%therefore processing takes about 0.2 gb/min
+%
+%100 Gb will take 8 hours.
+%
+%{
+line = 'C:\Users\zomege\Documents\Machine Learning\Matlab\Data\Line 06';
+tapes = dir(line);
+parfor i=(1:length(tapes))
+    name = tapes(i).name;
+    if(startsWith(name,'Tape','IgnoreCase',true))
+       path1 = strcat(line,delim,name,delim);
+       createCSV_tape(path1,P190,csv_location)
+    end
+end
+%}
