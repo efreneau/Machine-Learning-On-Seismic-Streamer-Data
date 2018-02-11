56f34f2692725f8837a16ca366870af2673e6ae5
efreneau <33821679+efreneau@users.noreply.github.com>
2018-02-10 17:20:59 -0800

    Add files via upload

--- Matlab/main.m
+++ Matlab/main.m
@@ -1,7 +1,7 @@
 P190 = 'C:\Users\zomege\Documents\Machine Learning\Matlab\P190\MGL1212MCS06.mat';
 csv_location = 'C:\Users\zomege\Documents\Machine Learning\Matlab';
 line = 'C:\Users\zomege\Documents\Machine Learning\Matlab\Data\Line 06';
-%path1 = 'C:\Users\zomege\Documents\Machine Learning\Matlab\Data\Line 06\TAPE0073.REEL\';
+path1 = 'C:\Users\zomege\Documents\Machine Learning\Matlab\Data\Line 06\TAPE0076.REEL\';
 
 if ispc %Choose path deliminator
     delim = '\';
@@ -9,12 +9,12 @@ else
     delim = '/';
 end
 
-%{
+
 tic
 createCSV_tape(path1,P190,csv_location)
 toc
-%}
 
+%{
 tapes = dir(line);
 for i=(1:length(tapes))
     name = tapes(i).name;
@@ -23,3 +23,4 @@ for i=(1:length(tapes))
        createCSV_tape(path1,P190,csv_location)
     end
 end
+%}
