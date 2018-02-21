a07235f2c345fa9af41fe3f97a82a1008e7fd8d2
efreneau <33821679+efreneau@users.noreply.github.com>
2018-02-20 23:45:32 -0800

    Add files via upload
    Added try-catch to catch the error when there is no nav data for a shot

--- Matlab/createCSV_tape.m
+++ Matlab/createCSV_tape.m
@@ -5,7 +5,12 @@ function createCSV_tape(path1,P190,csv_location)
         file = files(i).name;
         if(endsWith(file,'raw','IgnoreCase',true) && strcmp(file,'EOT.RAW')==0)
             location = strcat(path1,file);
-            createCSV(location,P190,csv_location);
+            try
+                createCSV(location,P190,csv_location);
+            catch
+                warning('Index exceeds matrix dimensions.');
+                disp('Out of bounds shot number. File Ignored.')
+            end
         end
     end
 end
