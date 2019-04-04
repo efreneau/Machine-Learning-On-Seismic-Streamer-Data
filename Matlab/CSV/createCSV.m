50ef709af9e31bfd68629a96600b12e4130dc12f
efreneau <efreneau@users.noreply.github.com>
2019-04-04 13:03:28 -0700

    RMS/SEL change

--- Matlab/CSV/createCSV.m
+++ Matlab/CSV/createCSV.m
@@ -1,17 +1,5 @@
 function createCSV(dataFile,P190,csv_dir)
-%createCSV(dataFile,P190,csv_dir)
-%
-%createCSV computes the RMS and SEL power for the siesmic streamer data and
-%outputs those and the navigation data found in the P190 to a CSV file. 
-%createCSV depends on readMCS, readP190 and readP190 being in the same
-%directory as this function to work.
-%
-%datafile is the raw streamer data. P190 is the navigation file. csv_dir is
-%the desired location for the output csv files.
-%
-%Example Usage:
-%createCSV('Matlab/Data/Line AT/R000179_1342879566.RAW','Matlab/P190/MGL1212NTMCS01.mat','Matlab');
-%createCSV('Matlab/Data/Line AT/R000179_1342879566.RAW','Matlab/P190/MGL1212NTMCS01.mat','Matlab');
+%createCSV('Z:\DATA\Line_05\TAPE0026.REEL\R000081_1342402331.RAW','D:\Machine Learning\Matlab\P190\MGL1212MCS05.mat','csvtest');
 %For more information see github.com/efreneau/machinelearninguw
     
     fs = 500;
@@ -23,7 +11,6 @@ function createCSV(dataFile,P190,csv_dir)
     end
     
     dataFileloc = strsplit(dataFile,delim);
-    %csv_dir = strcat(csv_dir,strcat(delim,'CSV',delim));%Add csv to the end
     csv_file = strcat(csv_dir,strjoin(dataFileloc(end-2:end),'_'));%'Line_Tape_File Name.csv'
     csv_file = strcat(csv_file(1:end-3),'csv');
     
@@ -36,8 +23,6 @@ function createCSV(dataFile,P190,csv_dir)
         return;
     end
  
-    fileID = fopen(csv_file,'w');
-    
     result = strjoin(dataFileloc(end-2:end),'_');
     result = strcat(result(1:end-3),'mat');%Make name for matlab data
     
@@ -50,70 +35,53 @@ function createCSV(dataFile,P190,csv_dir)
     disp(dataFile)
     readMCS(dataFile,P190,resultFile);
     load(resultFile);
-    f1 = Data1'*1e6;%unflipped
+    f1 = Data1'*1e6;
+    f1 = flip(f1,1);
     
     sos=[1,-2,1,1,-1.82570619168342,0.881881926844246;1,-2,1,1,-1.65627993129105,0.707242535896459;1,-2,1,1,-1.57205200320457,0.620422971870477];
     fData = sosfilt(sos,f1,2);%filter
     
     fData = db2mag(6)*fData; %Group length effect +6dB
     recievernum = size(fData,1);
-
-    T90 = zeros(5,recievernum);%Window size of 90% power
-    RMS = zeros(5,recievernum);%RMS Power
-    SEL = zeros(5,recievernum);%SEL Power
     
-    N = 2001;
-    freq = @(f) round(f*2*N/fs);%define frequencies
-    f10 = freq(10);
-    f40 = freq(40);
-    f70 = freq(70);
-    f100 = freq(100);
-    f110 = freq(110);
-    f140 = freq(140);
-    f170 = freq(170);
-    f200 = freq(200);
-
+    T90 = zeros(5,recievernum);%Window size of 90% power
+    RMS = zeros(5,recievernum);
+    SEL = zeros(5,recievernum);
+    peaks = zeros(1,recievernum);
+    ts_windowed = zeros(recievernum,4*fs+1);
+    e = zeros(recievernum,4*fs+1);%energy after filtering, windowed
+    
     parfor r=1:recievernum%Find RMS and SEL
-        row = fData(r,:);
-        [~,peak1] = max(row);
-        if peak1 <= 2*fs%Region 1: Peak is too close to the first index
-            DATA = row(1:peak1+2*fs).^2;%from peak1
-            DATA = [zeros(1,4*fs + 1 - length(DATA)),DATA];
-        elseif peak1 > 2*fs && length(row) - peak1>=2*fs%Region 2: Peak has space on either side
-            DATA = row(peak1-2*fs:peak1+2*fs).^2;
-        else %Region 3: Peak is too close to the end
-            DATA = row(peak1-2*fs:end).^2;
-            DATA = [DATA, zeros(1,4*fs + 1 - length(DATA))];
+        %row = fData(r,:);
+        [~,peaks(r)] = max(fData(r,:));
+        if peaks(r) <= 2*fs                                    %Region 1: Peak is too close to the first index
+            ts_windowed(r,:) = [zeros(1,4*fs + 1 - (peaks(r)+2*fs)),fData(r,1:peaks(r)+2*fs)];
+        elseif peaks(r) > 2*fs && length(fData(r,:)) - peaks(r)>=2*fs    %Region 2: Peak has space on either side
+            ts_windowed(r,:) = fData(r,peaks(r)-2*fs:peaks(r)+2*fs);
+        else                                                %Region 3: Peak is too close to the end
+            ts_windowed(r,:) = [fData(r,peaks(r)-2*fs:end), zeros(1,2*fs - (size(fData,2)-peaks(r)))];
         end
-        e_f = fft(DATA);
-        e = DATA;
+
         for band = 1:5%for each frequency band
             switch band
                 case 1%1: 10-110 Hz
-                    b1 = f10;
-                    b2 = f110;
-                    e = abs(ifft(fftshift([zeros(1,b1-1),e_f(b1:b2),zeros(1,N-b2)])));
+                    e(r,:) = bandpass(ts_windowed(r,:),[10, 110],fs).^2;
                 case 2%2: 40-140 Hz
-                    b1 = f40;
-                    b2 = f140;
-                    e = abs(ifft(fftshift([zeros(1,b1-1),e_f(b1:b2),zeros(1,N-b2)])));
+                    e(r,:) = bandpass(ts_windowed(r,:),[40, 140],fs).^2;
                 case 3%3: 70-170 Hz
-                    b1 = f70;
-                    b2 = f170;
-                    e = abs(ifft(fftshift([zeros(1,b1-1),e_f(b1:b2),zeros(1,N-b2)])));
+                    e(r,:) = bandpass(ts_windowed(r,:),[70, 170],fs).^2;
                 case 4%4: 100-200 Hz
-                    b1 = f100;
-                    b2 = f200;
-                    e = abs(ifft(fftshift([zeros(1,b1-1),e_f(b1:b2),zeros(1,N-b2)])));
+                    e(r,:) = bandpass(ts_windowed(r,:),[100, 200],fs).^2;
                 case 5%full: full band
-                    e=DATA;
+                    %no filtering
             end
-            T90(band,r) = t90(e);
-            RMS(band,r) = 10*log10(sum(e)/(2*fs*T90(band,r)));
+            T90(band,r) = t90(e(r,:));
+            RMS(band,r) = 10*log10(sum(e(r,:))/(2*fs*T90(band,r)));
             SEL(band,r) = RMS(band,r)+10*log10(T90(band,r)); 
         end
     end 
     
+    fileID = fopen(csv_file,'w');
     fprintf(fileID,'Date,Time,Depth of Airgun(m),Depth of Reciever(m),X Airgun,Y Airgun,Z Airgun,X_R1,Y_R1,Z_R1,SEL1,RMS1,SEL2,RMS2,SEL3,RMS3,SEL4,RMS4,SEL_full,RMS_full,T90_1,T90_2,T90_3,T90_4,T90_full\n');%column names
     for i = 1:recievernum %Append rows
         s = strcat(string(JulianDay),',',string(Time),',',string(Depth),',',string(receiver_depth(i)),',',string(X_Airgun),',',string(Y_Airgun),',',string(Z_Airgun),',',string(X_R1(i)),',',string(Y_R1(i)),',',string(Z_R1(i)),',',string(SEL(1,i)),',',string(RMS(1,i)),',',string(SEL(2,i)),',',string(RMS(2,i)),',',string(SEL(3,i)),',',string(RMS(3,i)),',',string(SEL(4,i)),',',string(RMS(4,i)),',',string(SEL(5,i)),',',string(RMS(5,i)),',',string(T90(1,i)),',',string(T90(2,i)),',',string(T90(3,i)),',',string(T90(4,i)),',',string(T90(5,i)),'\n');
@@ -123,7 +91,7 @@ function createCSV(dataFile,P190,csv_dir)
     disp(csv_file)
 end
 
-function tnin=t90(x)%t90 calculation for a 4s window
+function tnin=t90(x)%t90 calculation for a 4s window, takes energy as input
     fs = 500;
     index = 1;
     xsum = sum(x);
@@ -144,7 +112,4 @@ function tnin=t90(x)%t90 calculation for a 4s window
     end
     tnin = (n95-n05)/fs;%compute T90 = t95-t05
 end
-%createCSV('C:\Users\admin\Desktop\Machine Learning\Matlab\R000540_1342888533.RAW','C:\Users\admin\Desktop\Machine Learning\Matlab\P190\MGL1212NTMCS01.mat','C:\Users\admin\Desktop\Machine Learning\Matlab\test')
-%createCSV('C:\Users\admin\Desktop\Machine Learning\Matlab\R000095_1342877588.RAW','C:\Users\admin\Desktop\Machine Learning\Matlab\P190\MGL1212NTMCS01.mat','C:\Users\admin\Desktop\Machine Learning\Matlab\test2')
-
 
