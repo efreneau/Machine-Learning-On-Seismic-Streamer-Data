e084b90c088d5290477eba5ec9189a09e6df2784
efreneau <efreneau@users.noreply.github.com>
2019-04-16 13:36:46 -0700

    Ready for computations

--- Matlab/CSV/main.m
+++ Matlab/CSV/main.m
@@ -1,7 +1,5 @@
 P190 = 'D:\Machine Learning\Matlab\P190\MGL1212MCS11.mat';
 csv_location = 'D:\Machine Learning\Matlab\CSV1\Line_11_(RMS_and_SEL)\';%Testing_Data_(Line_05)
-%line = 'Y:\line4';
-%line = 'D:\Machine Learning\Matlab\Data\Line 06';
 line = 'Y:\machinelearninguw-master\Matlab\Data\Line 11';
 
 if ispc %Choose path deliminator
