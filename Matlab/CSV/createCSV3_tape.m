9bf3a966444f9981b8ca198dcb741d6ca182a32f
efreneau <efreneau@users.noreply.github.com>
2019-07-06 23:27:21 -0700

    Update createCSV3_tape.m

--- Matlab/CSV/createCSV3_tape.m
+++ Matlab/CSV/createCSV3_tape.m
@@ -1,4 +1,4 @@
-function createCSV3_tape(path1,P190,csv_location)
+ function createCSV3_tape(path1,P190,csv_location)
     files = dir(path1);
     parfor i = (1:length(files))
         file = files(i).name;
@@ -6,9 +6,10 @@ function createCSV3_tape(path1,P190,csv_location)
             location = strcat(path1,file);
             try
                 createCSV3(location,P190,csv_location);
-            catch
-                warning('Out of bounds shot number. File Ignored.');
-                delete(location)
+            catch er
+                rethrow(er)
+                %warning('Out of bounds shot number. File Ignored.');
+                %delete(location)
             end
         end
     end
