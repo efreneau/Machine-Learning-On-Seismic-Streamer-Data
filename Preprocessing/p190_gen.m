0d1220d793108862d0dc18bd4f94c7e20a5cfc94
efreneau <efreneau@uw.edu>
2019-11-24 15:19:07 -0800

    created 09abc line nav files

--- Preprocessing/p190_gen.m
+++ Preprocessing/p190_gen.m
@@ -20,8 +20,17 @@ nav = readP190('..\..\P190\MGL1110MCS07.p190')
 save('..\..\P190\MGL1110MCS07.mat')
 %}
 
-nav = readP190('C:\Users\zomege\Desktop\MGDS_Download\MGL1110MCS02.p190')
-save('..\..\P190\MGL1110MCS02.mat')
+nav = readP190('C:\Users\zomege\Desktop\MGDS_Download\MGL1212\MGL1212MCS09A.p190')
+save('C:\Users\zomege\Documents\GitHub\Machine-Learning-On-Seismic-Streamer-Data\Navigation_P190\MGL1212MCS09A.mat')
+clear all;
+
+nav = readP190('C:\Users\zomege\Desktop\MGDS_Download\MGL1212\MGL1212MCS09B.p190')
+save('C:\Users\zomege\Documents\GitHub\Machine-Learning-On-Seismic-Streamer-Data\Navigation_P190\MGL1212MCS09B.mat')
+clear all;
+
+nav = readP190('C:\Users\zomege\Desktop\MGDS_Download\MGL1212\MGL1212MCS09C.p190')
+save('C:\Users\zomege\Documents\GitHub\Machine-Learning-On-Seismic-Streamer-Data\Navigation_P190\MGL1212MCS09C.mat')
+clear all;
 
 function struct2vars(s)
 %STRUCT2VARS Extract values from struct fields to workspace variables
