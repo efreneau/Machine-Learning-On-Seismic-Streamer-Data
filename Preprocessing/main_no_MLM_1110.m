3bb2287547195f9f80bf477bb4f67c997f086be0
efreneau <efreneau@uw.edu>
2020-03-29 16:34:05 -0700

    add more lines to main script

--- Preprocessing/main_no_MLM_1110.m
+++ Preprocessing/main_no_MLM_1110.m
@@ -1,5 +1,6 @@
 %createCSV_no_MLM('C:\Users\zomege\Documents\GitHub\Machine-Learning-On-Seismic-Streamer-Data\example_shots\deep.RAW','C:\Users\zomege\Documents\GitHub\Machine-Learning-On-Seismic-Streamer-Data\Navigation_P190\Cascadia\MGL1212MCS05.mat','C:\Users\zomege\Documents\GitHub\Machine-Learning-On-Seismic-Streamer-Data\example_shots\deep_no_MLM.csv');
 
+%{
 line_name = 'Line_01a_no_MLM';
 line_dir = '/media/efreneau/a5c4b484-7df9-4b8d-940e-b5d31c6c9963/DATA/MGL1110/Line_01a/';
 csv_location = '/media/efreneau/a5c4b484-7df9-4b8d-940e-b5d31c6c9963/CSV/Line_01a_no_MLM/';
@@ -17,3 +18,59 @@ line_dir = '/media/efreneau/a5c4b484-7df9-4b8d-940e-b5d31c6c9963/DATA/MGL1110/Li
 csv_location = '/media/efreneau/a5c4b484-7df9-4b8d-940e-b5d31c6c9963/CSV/Line_05b_no_MLM/';
 P190 = '/home/efreneau/Downloads/Machine-Learning-On-Seismic-Streamer-Data-master/Navigation_P190/MGL1110/MGL1110MCS05B.mat';
 process_line_no_MLM(line_dir,csv_location,P190,line_name)
+%}
+
+%Make sure you change the p190
+
+line_name = 'Line_03_no_MLM';
+line_dir = '/media/efreneau/a5c4b484-7df9-4b8d-940e-b5d31c6c9963/DATA/MGL1110/Line_03/';
+csv_location = '/media/efreneau/a5c4b484-7df9-4b8d-940e-b5d31c6c9963/CSV/Line_03_no_MLM/';
+P190 = '/home/efreneau/Downloads/Machine-Learning-On-Seismic-Streamer-Data-master/Navigation_P190/MGL1110/MGL1110MCS03.mat';
+process_line_no_MLM(line_dir,csv_location,P190,line_name)
+
+line_name = 'Line_03T_no_MLM';
+line_dir = '/media/efreneau/a5c4b484-7df9-4b8d-940e-b5d31c6c9963/DATA/MGL1110/Line_03T/';
+csv_location = '/media/efreneau/a5c4b484-7df9-4b8d-940e-b5d31c6c9963/CSV/Line_03T_no_MLM/';
+P190 = '/home/efreneau/Downloads/Machine-Learning-On-Seismic-Streamer-Data-master/Navigation_P190/MGL1110/MGL1110MCS03T.mat';
+process_line_no_MLM(line_dir,csv_location,P190,line_name)
+
+line_name = 'Line_04_no_MLM';
+line_dir = '/media/efreneau/a5c4b484-7df9-4b8d-940e-b5d31c6c9963/DATA/MGL1110/Line_04/';
+csv_location = '/media/efreneau/a5c4b484-7df9-4b8d-940e-b5d31c6c9963/CSV/Line_04_no_MLM/';
+P190 = '/home/efreneau/Downloads/Machine-Learning-On-Seismic-Streamer-Data-master/Navigation_P190/MGL1110/MGL1110MCS04.mat';
+process_line_no_MLM(line_dir,csv_location,P190,line_name)
+
+line_name = 'Line_23a_no_MLM';
+line_dir = '/media/efreneau/a5c4b484-7df9-4b8d-940e-b5d31c6c9963/DATA/MGL1110/Line_23a/';
+csv_location = '/media/efreneau/a5c4b484-7df9-4b8d-940e-b5d31c6c9963/CSV/Line_23a_no_MLM/';
+P190 = '/home/efreneau/Downloads/Machine-Learning-On-Seismic-Streamer-Data-master/Navigation_P190/MGL1110/MGL1110MCS23A.mat';
+process_line_no_MLM(line_dir,csv_location,P190,line_name)
+
+line_name = 'Line_23b_no_MLM';
+line_dir = '/media/efreneau/a5c4b484-7df9-4b8d-940e-b5d31c6c9963/DATA/MGL1110/Line_23b/';
+csv_location = '/media/efreneau/a5c4b484-7df9-4b8d-940e-b5d31c6c9963/CSV/Line_23b_no_MLM/';
+P190 = '/home/efreneau/Downloads/Machine-Learning-On-Seismic-Streamer-Data-master/Navigation_P190/MGL1110/MGL1110MCS23B.mat';
+process_line_no_MLM(line_dir,csv_location,P190,line_name)
+
+line_name = 'Line_23c_no_MLM';
+line_dir = '/media/efreneau/a5c4b484-7df9-4b8d-940e-b5d31c6c9963/DATA/MGL1110/Line_23c/';
+csv_location = '/media/efreneau/a5c4b484-7df9-4b8d-940e-b5d31c6c9963/CSV/Line_23c_no_MLM/';
+P190 = '/home/efreneau/Downloads/Machine-Learning-On-Seismic-Streamer-Data-master/Navigation_P190/MGL1110/MGL1110MCS23C.mat';
+process_line_no_MLM(line_dir,csv_location,P190,line_name)
+
+%{
+line_name = 'Line_34_no_MLM';
+line_dir = '/media/efreneau/a5c4b484-7df9-4b8d-940e-b5d31c6c9963/DATA/MGL1110/Line_34/';
+csv_location = '/media/efreneau/a5c4b484-7df9-4b8d-940e-b5d31c6c9963/CSV/Line_34_no_MLM/';
+P190 = '/home/efreneau/Downloads/Machine-Learning-On-Seismic-Streamer-Data-master/Navigation_P190/MGL1110/MGL1110MCS34.mat';
+process_line_no_MLM(line_dir,csv_location,P190,line_name)
+%}
+
+line_name = 'Line_45_no_MLM';
+line_dir = '/media/efreneau/a5c4b484-7df9-4b8d-940e-b5d31c6c9963/DATA/MGL1110/Line_45/';
+csv_location = '/media/efreneau/a5c4b484-7df9-4b8d-940e-b5d31c6c9963/CSV/Line_45_no_MLM/';
+P190 = '/home/efreneau/Downloads/Machine-Learning-On-Seismic-Streamer-Data-master/Navigation_P190/MGL1110/MGL1110MCS45.mat';
+process_line_no_MLM(line_dir,csv_location,P190,line_name)
+
+
+
